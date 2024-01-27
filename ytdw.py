a91d5abefdb32ef9187cb018cf0aa3e38076073c
Liopol2 <leonardosoumas3@gmail.com>
2024-01-27 14:39:56 -0300

    Progress bar fixed Highest quality description

--- ytdw.py
+++ ytdw.py
@@ -3,11 +3,10 @@
 from tkinter import *
 from tkinter import ttk,filedialog
 import os,re
-from pytube import *
-from pytube.cli import on_progress
+from pytube import YouTube,Playlist
 
 #icecream es opcional, solo se esta usando para debuggear.
-#from icecream import ic
+from icecream import ic
 
 # Creating parent Tkinter window 
 app = Tk() 
@@ -52,17 +51,17 @@ def select_output(event):
 		qualitybox.current(4)
   
 #Output choice
-outputlabel=Label(text='Output',font=20).grid(row=0, column=0, padx=10, pady=5, sticky="NESW")#.pack()
+outputlabel=Label(text='Output',font=20).grid(row=0, column=0, padx=10, pady=5, sticky="NESW")
 outputbox=ttk.Combobox(app,value=outputoptions)
 outputbox.current(0)
-outputbox.grid(row=0, column=1, padx=10, pady=5, sticky="NESW")#.pack(pady=20)
+outputbox.grid(row=0, column=1, padx=10, pady=5, sticky="NESW")
 outputbox.bind("<<ComboboxSelected>>",select_output)
 
 #Quality Option
-Label(app, text='Quality', font=20).grid(row=1, column=0, padx=10, pady=5, sticky="NESW")#.pack()
+Label(app, text='Quality', font=20).grid(row=1, column=0, padx=10, pady=5, sticky="NESW")
 qualitybox=ttk.Combobox(app,value=audioqualityoptions)
 qualitybox.current(4)
-qualitybox.grid(row=1, column=1, padx=10, pady=5, sticky="NESW")#.pack(pady=20)
+qualitybox.grid(row=1, column=1, padx=10, pady=5, sticky="NESW")
 
 
 #Output Folder
@@ -76,7 +75,8 @@ def cambiardestino(e):
   
 folderlabel=Label(text=f"📁 {folder}",font=25,cursor="hand1")
 folderlabel.bind('<1>',cambiardestino)
-folderlabel.grid(row=2, column=0,columnspan=2, padx=10, pady=5, sticky="NESW")#.pack()
+folderlabel.grid(row=2, column=0,columnspan=2, padx=10, pady=5, sticky="NESW")
+
 #Link
 def clearentry(event):
 	link.config(fg='white')
@@ -89,67 +89,85 @@ def validate_link(event):
 	if 'playlist' in url:
 		playlist=Playlist(url)
 		for stream in playlist.videos:
-			stream.register_on_progress_callback(on_progress)
-			#ic(f'descargando {stream.title} de {playlist.title}')
-			downloadstream(stream) 
+			video= YouTube(stream.watch_url,on_progress_callback=on_progress)
+			getstream(video) 
 	#If this is only 1 video
 	else:
-		stream=YouTube(url)
-		#ic(f'Video: {stream.title}')
-		downloadstream(stream)
+		stream=YouTube(url,on_progress_callback=on_progress)
+		getstream(stream)
 
-def downloadstream(video):
-	bytes_received=0
+#Progressbar method
+def on_progress(stream,chunk,bytes_remaining):
+	total_size = stream.filesize
+	bytes_downloaded = total_size - bytes_remaining
+	percentage= bytes_downloaded/ total_size * 100
+	pb['value']=percentage
+
+def fileexist(filename):
+	content.set(f'{filename} ya existe')
+	link.config(fg='yellow')
+	link.config(bg='black')
+
+def downloadstream(title,stream,quality="Best Quality"):
+	stream.download(output_path=folder)
+	content.set(f'{title} {quality} ✔')
+	link.config(foreground='green')
+
+def getstream(video):
 	quality=qualitybox.get()	
 	output=outputbox.get()
-	if output == 'Audio':
-		if os.path.exists(f'{folder}/{video.title}.webm'):
-			content.set(f'{video.title} ya existe')
-			link.config(fg='yellow')
-			link.config(bg='black')
-		else:
-			#este regex quita lo que esta dentro del parentesis
-			qualityval = re.sub(r'\([^)]*\)', '', quality)
-			#ic(qualityval)
-			a=video.streams.filter(only_audio=True,abr=qualityval)
-			if a:      
-				a.first().download(output_path=folder)
-				content.set(f'{video.title} {qualityval} ✔')
-				link.config(foreground='green')
-			else: 
-				content.set(f'Intente con calidad distinta a {quality}')
-				link.config(foreground='red')
-	elif output == 'Video':
-		if os.path.exists(f'{folder}/{video.title}.mp4'):
-			content.set(f'{video.title} ya existe')
-			link.config(fg='yellow')
-		else:
-			if quality == 'Highest':
-				v=video.streams.filter(progressive=True).order_by("resolution").first()
-				content.set(f'descagado {video} a la mejor calidad')
-				v.download(output_path=folder)
-				link.config(foreground='green')
-			elif quality:			
-					v=video.streams.filter(progressive=True,res=quality).first()
-					filename=f'{video.title}.webm'
-					#ic(video,filename)
-					if v:
-						if filename in folder:						
-							content.set(f'⬇ {video.title}')
-						v.download(output_path=folder)
-						content.set(f'{video.title} {quality} ✔')
-						link.config()
-					else:
-						content.set(f'Intente con calidad distinta a {quality}')
-						link.config(foreground='red')
+	title=video.title
+	match output:
+		case 'Audio':
+			if os.path.exists(f'{folder}/{video.title}.webm'):
+				fileexist(title)
+			else:
+				#este regex quita lo que esta dentro del parentesis
+				qualityval = re.sub(r'\([^)]*\)', '', quality)
+				#ic(qualityval)
+				stream = video.streams.filter(only_audio=True,abr=qualityval).first()
+				if stream:      
+					downloadstream(title,stream,qualityval)
+				else: 
+					content.set(f'Intente con calidad distinta a {quality}')
+					link.config(foreground='red')
+	
+		case 'Video':
+			if os.path.exists(f'{folder}/{video.title}.mp4'):
+				fileexist(title)
+			else:
+				if quality == 'Highest':
+					stream=video.streams.get_highest_resolution() 
+					downloadstream(title,stream)
+				elif quality:
+					#No entiendo porque pero esto tiene que estar en dos lineas. 			
+					v=video.streams.filter(progressive=True,res=quality)
+					stream=v.first()
+					downloadstream(title,stream,quality)
+				else:
+					content.set(f'Intente con calidad distinta a {quality}')
+					link.config(foreground='red')
      
 content=StringVar(app,'Insert link here')
 link = Entry(width=31, font='Arial 30',justify='center',textvariable=content)
 link.config(bg='grey',fg='white')
 link.bind('<1>',clearentry)
 link.bind('<Key-Return>',validate_link)
-link.grid(row=3, column=0,columnspan=2, padx=10, pady=5, sticky="NESW")#.pack()
+link.grid(row=3, column=0,columnspan=2, padx=10, pady=5, sticky="NESW")
+
+#Progressbar
+pb = ttk.Progressbar(
+    app,
+    orient='horizontal',
+    mode='determinate',
+    length=280
+)
+
+pb.grid(column=0, row=4, columnspan=2, padx=10, pady=5,sticky="NESW")
+
 app.grid_rowconfigure(0, weight=1)
 app.grid_rowconfigure(1, weight=1)
 app.grid_rowconfigure(2, weight=1)
+app.grid_rowconfigure(3, weight=1)
+app.grid_rowconfigure(4, weight=1)
 mainloop() 
